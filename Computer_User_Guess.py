bf315a89c7a707fdae3a09e458032cb9bc03a982
Neda Peiravian <neda.peiravian@gmail.com>
2020-12-21 10:05:30 -0800

    Update Computer_User_Guess.py

--- Computer_User_Guess.py
+++ Computer_User_Guess.py
@@ -7,7 +7,6 @@ print("Choose a number between 1 and 100 in your mind: ")
 print("I guessed" , number)
 FeedBack = str(input("Please give me FeedBack : "))
 count = 1
-#while(FeedBack != 'C' and FeedBack != 'c'):   # This line is equivalent of the line below:
 while(not(FeedBack == 'C' or FeedBack == 'c')):
     count += 1
     if (FeedBack != 'S' and FeedBack != 's'):
